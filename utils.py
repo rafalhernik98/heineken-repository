3c72f7a491f418aef7e072a8e4c914715b764ece
rafalhernik98 <rafalhernik98@gmail.com>
2024-10-15 21:42:06 +0200

    Add docstring to clean_demand_per_group column

--- utils.py
+++ utils.py
@@ -7,8 +7,10 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
+
 def parse_time(s):
     return datetime.strptime(s, "%Y-%m-%d").date()
+
 
 def read_demand(path):
     df = pd.read_csv(path)
@@ -17,6 +19,7 @@ def read_demand(path):
     df.index = pd.DatetimeIndex(df.index)
     return df
 
+
 def read_promotions(path):
     df = pd.read_csv(path, index_col=0)
     df = df.assign(promotion_date=lambda df: df.promotion_date.apply(parse_time))
@@ -24,18 +27,37 @@ def read_promotions(path):
     df.index = pd.DatetimeIndex(df.index)
     return df
 
+
 def clean(ts: pd.Series) -> pd.Series:
     # Replaces missing values
     return ts.bfill().fillna(ts.mean())
 
+
 def clean_demand_per_group(demand: pd.DataFrame) -> pd.DataFrame:
-    """TODO add docstring"""
+    """Cleans the demand data for each unique (supermarket, SKU) pair by filling in missing values.
+    
+    The function is iterating over unique combinations of (supermarket, SKU) pairs in the provided 
+    'demand' DataFrame. For each of such combination, it applies the 'clean' function to the 'demand' column.
+
+    Parameters
+    ----------
+    demand : pd.DataFrame
+        A DataFrame containing the demand data
+
+    Returns
+    ----------
+    pd.DataFrame
+        The cleand input DataFrame with filled missing values with the "demand" column
+            cleaned for each supermarket and SKU combination.
+    """
     sus = demand.supermarket.unique()
     skus = demand.sku.unique()
     for su in sus:
         for sku in skus:
-            demand.loc[(demand.sku == sku) & (demand.supermarket == su), "demand"] = clean(demand.loc[(demand.sku == sku) & (demand.supermarket == su), "demand"])
+            demand.loc[(demand.sku == sku) & (demand.supermarket == su), "demand"] = clean(
+                demand.loc[(demand.sku == sku) & (demand.supermarket == su), "demand"])
     return demand
+
 
 def merge(demand: pd.DataFrame, promotions: pd.DataFrame) -> pd.DataFrame:
     promotions = promotions.rename_axis("date").assign(promotion=True)
@@ -46,6 +68,7 @@ def merge(demand: pd.DataFrame, promotions: pd.DataFrame) -> pd.DataFrame:
     )
     demand = demand.assign(promotion=lambda df: df.promotion.fillna(False))
     return demand
+
 
 def extend_promotions_days(promotions, n_days):
     """ Extends the promotions to have multiple rows for a specific number of days.
@@ -63,8 +86,9 @@ def extend_promotions_days(promotions, n_days):
         extended_promotions = extended_promotions.append(additional_promotion_days)
     return extended_promotions
 
+
 def aggregate_to_weekly(df):
-    grouped = df.groupby(["sku", "supermarket"]) 
+    grouped = df.groupby(["sku", "supermarket"])
     # Performs a simplistic aggregation of promotion. If a promotion occured during the week this variable will be true.
     weekly = grouped.apply(lambda df: df.resample("W").agg({"demand": "sum", "promotion": "max"}))
     weekly = weekly.reset_index().set_index("date")
